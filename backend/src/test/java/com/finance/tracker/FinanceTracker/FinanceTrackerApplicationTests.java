6839100ac27d6e9afaaec8fc33509ddde9cc8863
arbersc <arber@scope.com.mt>
2025-04-12 02:13:06 +0200

    Add Java backend project

---
+++ backend/src/test/java/com/finance/tracker/FinanceTracker/FinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.tracker.FinanceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
